a365c8113cf53a101de932dbff8b961181125ce0
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2026-09-23 07:26:26 +0500

    let , var , const variables

---
+++ index.js
@@ -0,0 +1,14 @@
+let age = 20;
+age = 30;
+console.log("Age:", age);
+
+var name = "John";
+name = "Doe";
+var name = 20;
+console.log("Name:", name);
+
+const city = "New York";
+// city = "Los Angeles"; // This will throw an error because 'city' is a constant
+city = "Los Angeles"; // This will throw an error because 'city' is a constant
+console.log("City:", city);
+
