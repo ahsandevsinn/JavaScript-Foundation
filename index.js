3b4b9ae37e942b2788e1a6214c2be69a2557b46a
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2026-09-26 07:15:38 +0500

    Declaration, Assignment, Reassignment, Variable Naming

--- index.js
+++ index.js
@@ -3,11 +3,40 @@
 
 // what is var ?
 // var is a keyword that can change variable value. and same variable assign multiple times. var is using as an old javascript.
+// var name = "Ahsan";
+// console.log(name);
+// var name = "Ahsan Khan";
+// console.log(name);
 
 // what is let ?
 // let is a keyword that can change variable value. and same variable value can be changed but not redeclared. let is using as a new javascript.
+// let age = 20;
+// console.log(age);
+// age = 21;
+// console.log(age);
+
 
 // what is const ?
 // const is a keyword that cannot change variable value. and same variable value cannot be changed or redeclared. const is using as a new javascript.
+// const country = "Pakistan";
+// console.log(country);
+// country = "India"; // This will throw an error because const cannot be reassigned.           
 
+// Declaration
+// let age;
+// // Initialization
+// age = 20;
 
+// Assignment
+let age = 20; // Declaration and Initialization
+console.log(age); // Output: 20
+
+// Reassignment
+age = 21;
+console.log(age); // Output: 21
+
+// Variable Naming
+let userAge = 25; // Valid variable name
+// let 1userAge = 30; // Invalid variable name (cannot start with a number)
+// let user-age = 35; // Invalid variable name (cannot contain hyphens)
+// let userAge! = 40; // Invalid variable name (cannot contain special characters except $ and _)
